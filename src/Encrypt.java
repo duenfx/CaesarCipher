751ee52a9337050de0b7b7d00f5e4bbc89ce6535
Rina <karinahanzha@gmail.com>
2025-10-25 15:43:38 +0300

    add encryptText() method for Caesar Cipher

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -3,5 +3,16 @@ public class Encrypt {
     public static char[] getAlphabet() {
         return alphabet;
     }
-
+    public static String encryptText(String text, int key) {
+        char[] symbol = text.toCharArray();
+        for (int i = 0; i < symbol.length; i++) {
+            for (int j = 0; j < alphabet.length; j++) {
+                if (symbol[i] == alphabet[j]) {
+                    symbol[i] = alphabet[(j + key) % alphabet.length];
+                    break;
+                }
+            }
+        }
+        return new String(symbol);
+    }
 }
