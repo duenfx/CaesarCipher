592b7747750f23e10f6943163e1795eee20b84af
Rina <karinahanzha@gmail.com>
2025-10-25 15:54:38 +0300

    add decryptText() method for Caesar Cipher in Decrypt.java

--- src/Decrypt.java
+++ src/Decrypt.java
@@ -1,3 +1,15 @@
 public class Decrypt {
-
+    public static String decryptText(String text, int key) {
+        char[] symbol = text.toCharArray();
+        char[] alphabet = Encrypt.getAlphabet();
+        for (int i = 0; i < symbol.length; i++) {
+            for (int j = 0; j < alphabet.length; j++) {
+                if (symbol[i] == alphabet[j]) {
+                    symbol[i] = alphabet[((j - key) + alphabet.length) % alphabet.length];
+                    break;
+                }
+            }
+        }
+        return new String(symbol);
+    }
 }
