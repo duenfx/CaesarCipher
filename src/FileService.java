b414a936a11ca345b0e0787e54e2c89efc313161
Rina <karinahanzha@gmail.com>
2025-10-26 09:49:56 +0200

    add readFile() method in FileService.java

--- src/FileService.java
+++ src/FileService.java
@@ -1,2 +1,9 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileService {
+    public static String readFile(Path path) throws IOException {
+        return Files.readString(path);
+    }
 }
