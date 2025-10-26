2f33894a615cc277851ebac6b1856ccdf92b9c44
Rina <karinahanzha@gmail.com>
2025-10-26 17:12:42 +0200

    add new logic in Decrypt.java

--- src/Decrypt.java
+++ src/Decrypt.java
@@ -3,11 +3,16 @@ public class Decrypt {
         char[] symbol = text.toCharArray();
         char[] alphabet = Encrypt.getAlphabet();
         for (int i = 0; i < symbol.length; i++) {
+            boolean found = false;
             for (int j = 0; j < alphabet.length; j++) {
                 if (symbol[i] == alphabet[j]) {
                     symbol[i] = alphabet[((j - key) + alphabet.length) % alphabet.length];
+                    found = true;
                     break;
                 }
+            }
+            if (!found) {
+                symbol[i] = symbol[i];
             }
         }
         return new String(symbol);
