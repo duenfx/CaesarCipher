f69248619a7bcc54e3c49c21b5dabf4dc53f182c
Rina <karinahanzha@gmail.com>
2025-10-26 10:34:18 +0200

    add isReadable() method in BruteForce.java

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,2 +1,13 @@
 public class BruteForce {
+    private boolean isReadable(String text) {
+        text = text.toLowerCase();
+        String[] commonWords = {" the ", " and ", " is ", " of ", " to ", " in ", " it ", " that ", " was "};
+        int matches = 0;
+        for (String word : commonWords) {
+            if (text.contains(word)) {
+                matches++;
+            }
+        }
+        return matches >= 3;
+    }
 }
