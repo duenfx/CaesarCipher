2e056d9cb37e7be383f64e964a1b2e23ea8def33
Rina <karinahanzha@gmail.com>
2025-10-26 17:13:01 +0200

    add new logic in Encrypt.java

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -6,11 +6,16 @@ public class Encrypt {
     public static String encryptText(String text, int key) {
         char[] symbol = text.toCharArray();
         for (int i = 0; i < symbol.length; i++) {
+            boolean found = false;
             for (int j = 0; j < alphabet.length; j++) {
                 if (symbol[i] == alphabet[j]) {
                     symbol[i] = alphabet[(j + key) % alphabet.length];
+                    found = true;
                     break;
                 }
+            }
+            if (!found) {
+                symbol[i] = symbol[i];
             }
         }
         return new String(symbol);
